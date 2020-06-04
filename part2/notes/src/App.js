74dc344b6713e094f3b83249911c5ed525711c40
unknown <ameliaggwp@gmail.com>
2020-06-03 17:01:34 -0700

    Saving progress

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./components/Note.js";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toDateString,
+    };
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note, i) => (
-          <Note key={i} note={note} />
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
