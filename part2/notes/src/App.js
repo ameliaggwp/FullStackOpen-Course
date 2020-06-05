cf4fc6b0bdf6f5317df86fbc42d8360fd2b84e6b
unknown <ameliaggwp@gmail.com>
2020-06-04 20:00:04 -0700

    Part two notes example completed

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -4,13 +4,18 @@ import Note from "./components/Note.js";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
-      date: new Date().toDateString,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
     };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
 
   const handleNoteChange = (event) => {
@@ -18,11 +23,19 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
+  // If value is showAll, it shows all notes, else it filters for notes that are marked as important
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
