d74b3be48963e580f6a7a7cdae90152908b2ddc1
unknown <ameliaggwp@gmail.com>
2020-06-05 10:21:57 -0700

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,41 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Harry Potter" }]);
-
   const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === personObject.name) {
+        return alert(`${newName} is already added to phonebook`);
+      }
+    }
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          Name: <input />
+          Name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   );
 };
