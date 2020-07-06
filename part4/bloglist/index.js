58d5e252562186cfbb262cbe180d481ce12ca746
unknown <ameliaggwp@gmail.com>
2020-07-05 18:12:16 -0700

    Creating modules for 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,10 +1,12 @@
-const http = require("http")
+const { port, mongodb_uri } = require("./utils/config")
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const logger = require("./utils/logger")
 const mongoose = require("mongoose")
-const { port, mongodb_uri } = require("./utils/config")
+const blogsRouter = require("./controllers/blogs")
 
+app.use("/api/blogs", blogsRouter)
 app.use(cors())
 app.use(express.json())
 
@@ -14,54 +16,12 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to MongoDB")
+    logger.info("Connected to MongoDB")
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB", error.message)
+    logger.error("error connecting to MongoDB", error.message)
   })
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
-
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
-
-app.get("/api/blogs", (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (req, res) => {
-  const body = req.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  })
-
-  blog.save().then((result) => {
-    res.status(201).json(result)
-  })
-})
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  logger.info(`Server running on port ${port}`)
 })
