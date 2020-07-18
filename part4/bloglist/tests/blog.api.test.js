1a8c0a2261a1f0dc2b30361eb4abf271cde9e068
unknown <ameliaggwp@gmail.com>
2020-07-18 16:36:37 -0700

    4.10 Blog list tests, step3

--- part4/bloglist/tests/blog.api.test.js
+++ part4/bloglist/tests/blog.api.test.js
@@ -14,18 +14,42 @@ beforeEach(async () => {
   }
 })
 
-test("Total blogs equals correct amount of blogs in DB", async () => {
-  const res = await api.get("/api/blogs")
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
-})
+describe("Blogs", () => {
+  test("Total blogs equals correct amount of blogs in DB", async () => {
+    const res = await api.get("/api/blogs")
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
-})
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+  })
 
+  test("Unique identifier property of blog is named id", async () => {
+    const res = await api.get("/api/blogs")
+
+    expect(res.body[0].id).toBeDefined()
+  })
+
+  test("blog is successfully created and added to database", async () => {
+    const newBlog = {
+      title: "BTS sweeps Melon Charts",
+      author: "Lia D",
+      url: "www.btsblog.com",
+      likes: 1000000,
+    }
+
+    await api.post("/api/blogs").send(newBlog).expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map((b) => b.title)
+    expect(titles).toContain("BTS sweeps Melon Charts")
+  })
+})
 afterAll(() => {
   mongoose.connection.close()
 })
