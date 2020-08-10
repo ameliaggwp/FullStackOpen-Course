24b2cdb644ee806abade5e8c7c49089f9e7631d2
unknown <ameliaggwp@gmail.com>
2020-08-10 14:04:12 -0700

    Adding user implementation

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,12 +8,14 @@ const logger = require("./utils/logger")
 const cors = require("cors")
 const middleware = require("./utils/middleware")
 const blogsRouter = require("./controllers/blogs")
+const usersRouter = require("./controllers/users")
 
 app.use(express.json())
 app.use(cors())
 app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
