927b7c65e2c34f43b83ba34c9c7eefcd28d4d067
unknown <ameliaggwp@gmail.com>
2020-08-12 13:52:01 -0700

    Linking blogs and users so you can see relevant info in both get requests

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,29 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
   res.json(blogs)
 })
 
 blogsRouter.post("/", async (req, res) => {
   const body = req.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
-    author: body.author,
+    author: user.username,
     url: body.url,
     likes: body.likes,
+    user: user._id,
   })
 
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+
   res.json(result)
 })
 
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose")
 const uniqueValidator = require("mongoose-unique-validator")
+
+mongoose.set("useFindAndModify", false)
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
